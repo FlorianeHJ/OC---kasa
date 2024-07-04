4f02a41374a8cd67b6371d278006c3841d7f8ce9
Floriane HJ <fjulia.dev@gmail.com>
2024-07-04 15:51:49 +0200

    Création de route
    3 pages créées

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+        incidunt laudantium deleniti aperiam quia labore iste eveniet laboriosam
+        ipsum voluptate, sunt officiis deserunt, a necessitatibus provident,
+        quaerat iusto vel voluptatem!
+      </p>
+    </div>
+  );
+};
+
+export default About;
