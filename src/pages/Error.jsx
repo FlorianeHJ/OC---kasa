5143d3162246f2c5241885aaca884d352a06148f
Floriane HJ <fjulia.dev@gmail.com>
2024-07-08 15:09:49 +0200

    Page d'erreur terminée

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div  >
+      <Header />
+      <div className="error">
+      <h1 >404</h1>
+      <p>Oups ! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="linkError">Retourner vers la page d'accueil</Link></div>
+      <Footer />
     </div>
   );
 };
