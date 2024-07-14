6e3c78cb743a51630eb0d401fbcb2df4cd5f6735
Floriane HJ <fjulia.dev@gmail.com>
2024-07-14 15:26:57 +0200

    début collapse

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Collapse = ({title, details}) => {
+
+const [isCollapse, setIsCollapse] = useState(true)
+
+const toggleCollapse = () => {
+    setIsCollapse(!isCollapse)
+}
+
+    return (
+        <div >
+            <div onClick={toggleCollapse} className='collapse'>
+            <h3 className='collapse-title'>{title}</h3>
+            <span className='collapse-icon'>{isCollapse ? <i className="fa-solid fa-chevron-down"></i> : <i className="fa-solid fa-chevron-up"></i>}</span>
+            </div>
+            {!isCollapse && (
+            <div>{details}</div>
+        )}
+        </div>
+        
+    );
+};
+
+export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,16 +2,32 @@ import React from "react";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Collapse from "../components/Collapse";
 
 
 const About = () => {
+
+   const details1 = <p>Les annonces postées sur Kasa garantissetn une fiabilité totale. Les phootos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+
+   const details2 = <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+
+   const details3 = <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+
+   const details4 = <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers domestique pour nos hôtes.</p>
+
   return (
     <div>
       <Header />
       <div className="img-container">
-   
-   </div>
+      </div>
+<div>
+<Collapse title="Fiabilité" details={details1} />
+<Collapse title="Respect" details={details2} />
+<Collapse title="Service" details={details3} />
+<Collapse title="Securité" details={details4} />
 
+</div>
+     
       <Footer />
     </div>
   );
