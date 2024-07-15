47cbd1de35c4f0186b0d28c8c3471778d7a593df
Floriane HJ <fjulia.dev@gmail.com>
2024-07-15 16:29:23 +0200

    Création des cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Card from "../components/Card";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import data from "../data/appartements.json";
 
 
 const Home = () => {
+
+
+
   return (
     <div className="container">
       <Header />
 
       <div className="content">
-      <div className="home-headers">
-        <p></p>
+      <div className="home-banner">
+        <p>Chez vous, partout et ailleurs</p>
+      </div>
+      <div className="container-cards">
+        {data.map(card => (<Card key={card.id} title={card.title} cover={card.cover} />))}
+        
       </div>
 </div>
       <Footer />
