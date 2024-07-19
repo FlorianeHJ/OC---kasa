fbb878efca930f75e96333f6941530ee6b710538
Floriane HJ <fjulia.dev@gmail.com>
2024-07-19 16:30:30 +0200

    Ajout logo redirection accueil

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Logo = () => {
     return (
         <div > 
-        <img src={logo} alt="Logo Kasa" className="logo"/>
+            <Link to="/"><img src={logo} alt="Logo Kasa" className="logo"/></Link>
+        
             
         </div>
     );
